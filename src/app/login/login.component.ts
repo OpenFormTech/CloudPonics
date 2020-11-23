b2ed8c1bbe9d81c26f2ecd06908a38a874717d3c
liamm91 <liamm91@kprschools.ca>
2020-11-23 16:12:12 -0500

    added simple auth to the user login with redirects

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+import firebase from 'firebase/app';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  user: firebase.User | null;
 
-  constructor() { }
+  constructor(public auth: AuthService, public router: Router) { }
 
   ngOnInit(): void {
+    this.getUser();
   }
 
+  getUser(): void{
+    this.auth.listenForUser(user => {
+      this.user = user;
+      if ( !!this.user ) {
+        // if there is a user signed in, navigate to dashboard
+        console.log("received auth'd user, switching page");
+        this.router.navigate(['/dashboard']);
+      }
+    });
+  }
 }
