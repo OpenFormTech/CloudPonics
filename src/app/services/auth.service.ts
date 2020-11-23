ddf829baa3160f34e8a4b8ea8b75eafa116e339c
liamm91 <liamm91@kprschools.ca>
2020-11-23 16:12:56 -0500

    added more GitHub and email sign in methods. added a subscription method to centralize code into the service. edited comments

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -7,8 +7,8 @@ import { AngularFireAuth } from '@angular/fire/auth';
 import firebase from 'firebase/app';
 
 // rxJS imports
-import { Observable, of } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { Observable, of, Subscription } from 'rxjs';
+import { switchMap, tap } from 'rxjs/operators';
 
 /**
  * what are observables?
@@ -23,14 +23,19 @@ import { switchMap } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class AuthService {
-  user$: Observable<firebase.User>;
+  // configuring the observable for typescript
+  user$: Observable<firebase.User | null>;
 
   constructor(
     // required inputs to the constructor
     private afAuth: AngularFireAuth,
     private router: Router
+
   ) { 
-    // defining our user from the authstate and returning it as an observable
+    /**
+     * using storing the user as an observable which essentially turns it into a datastream,
+     * which can be listened to from different parts of code.
+    */
     this.user$ = this.afAuth.authState.pipe(
       /**
        * using rxJS magic to return the observable of the user if the user
@@ -38,8 +43,10 @@ export class AuthService {
        */
       switchMap(user => {
         if ( user ) {
+          // console.log("there is a auth'd user");
           return of(user);
         } else {
+          // console.log("there is a null user");
           return of(null);
         }
       })
@@ -53,9 +60,34 @@ export class AuthService {
     return credential.user;
   }
 
+  // creating the github sigin in option
+  async githubSignIn(){
+    const provider = new firebase.auth.GithubAuthProvider;
+    const credential = await this.afAuth.signInWithPopup(provider);
+    return credential.user;
+  }
+
+  // creating the github sigin in option
+  async emailSignIn(){
+    const provider = new firebase.auth.EmailAuthProvider;
+    const credential = await this.afAuth.signInWithPopup(provider);
+    return credential.user;
+  }
+
   // basic sign out function
   async signOut() {
     await this.afAuth.signOut();
+    // console.log("signed out of account");
     return this.router.navigate(['/']);
+  }
+
+  listenForUser(tapFunc: (result: firebase.User | null) => void): Subscription{
+    const l = this.user$.pipe(
+      tap(async u => tapFunc(u))
+    ).subscribe(
+      // leaving open for future possible changes
+    )
+
+    return l;
   }
 }
