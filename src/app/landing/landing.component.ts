515e31bc468f332a80d5a86396864ee256b02d55
Liam Major <liam.major91@gmail.com>
2020-11-27 22:18:47 -0500

    pruned auth code

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -13,7 +13,7 @@ import firebase from 'firebase/app';
 export class LandingComponent implements OnInit {
   user: firebase.User | null; // making a variable for the user
   
-  constructor(config: NgbCarouselConfig, public auth: AuthService, public router: Router) {
+  constructor(config: NgbCarouselConfig) {
     // customize default values of carousels used by this component tree
     config.interval = 3000;
     config.wrap = true;
@@ -24,7 +24,6 @@ export class LandingComponent implements OnInit {
   }
   
   ngOnInit(): void {
-    this.getUser();
   }
   
   images = [
@@ -32,32 +31,6 @@ export class LandingComponent implements OnInit {
     {src: '../../assets/images/carousel1.png', desc: 'Fleet deployability.', link:"#"},
     {src: '../../assets/images/carousel2.jpg', desc: 'Research applicability.', link:"#"}, 
   ];
-
-  async getUser() {
-    this.auth.listenForUser(user => {
-      this.user = user;
-    });
-  }
-
-  /**
-   * this is the payload functionized and factored out of async getUser(),
-   * so that a user can hit the login button and be transported to the right page.
-   */
-  testUserState() {
-    /**
-     * this payload is to redirect the user to the specified pages
-     */
-
-    if ( !!this.user ) {
-      // if there is a user signed in, navigate to dashboard
-      console.log("received null user, switching page");
-      this.router.navigate(['/dashboard']);
-    } else {
-      // if there is no user signed in, navigate to login
-      console.log("received null user, switching page");
-      this.router.navigate(['/login']);
-    }
-  }
   
   @ViewChild('carousel', {static : true}) carousel: NgbCarousel;
   
