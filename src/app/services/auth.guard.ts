130bf30821ed378ca194fa937906e171fc9d729d
Liam Major <liam.major91@gmail.com>
2020-11-27 22:31:22 -0500

    added comments to the guard and route to login page if there is no auth'd user

--- src/app/services/auth.guard.ts
+++ src/app/services/auth.guard.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable, of } from 'rxjs';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
 import { map, take, tap } from 'rxjs/operators';
 import { AuthService } from './auth.service';
 
@@ -8,18 +8,23 @@ import { AuthService } from './auth.service';
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  constructor(private auth: AuthService) { }
+  constructor(private auth: AuthService, private router: Router) { }
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> 
   {
     return this.auth.user$.pipe(
-      take(1),
-      map(user => !!user),
+      take(1), // take the first value emitted
+      map(user => !!user), // change the value emitted
       tap(loggedIn => {
-        if(loggedIn) {
-          console.log("unauth'd user tried to access")
+        /**
+         * copy it and log the value for debug purposes, not required and has no effect on the *returned* value,
+         * the effect this code has is it makes the router redirect to the home page.
+         */
+        if(!loggedIn) {
+          console.log("unauth'd user tried to access");
+          this.router.navigate(['/login'])
         }
       })
       // returns the mapped boolean user
