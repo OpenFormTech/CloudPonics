60636b08845883610c375e8e50a32e7a7c1727ab
Liam Major <liam.major91@gmail.com>
2020-11-27 22:30:58 -0500

    added the guard to the dashboard route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './services/auth.guard';
 
 const routes: Routes = [
   { path: "", pathMatch: "full", redirectTo: "landing"},
   { path: "landing", pathMatch: "full", loadChildren: async () => (await import('./landing/landing.module')).LandingModule },
   { path: "login", pathMatch: "full", loadChildren: async () => (await import('./login/login.module')).LoginModule, },
-  { path: "dashboard", pathMatch: "full", loadChildren: async () => (await import('./dashboard/dashboard.module')).DashboardModule, },
+  { path: "dashboard", pathMatch: "full", loadChildren: async () => (await import('./dashboard/dashboard.module')).DashboardModule, canActivate: [AuthGuard]},
 ];
 
 @NgModule({
