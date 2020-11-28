c4ab785f5ad0eb2c450c478c34bf92fb500a3373
Liam Major <liam.major91@gmail.com>
2020-11-27 22:23:33 -0500

    added auth state logic with rxjs

--- src/app/services/auth.guard.ts
+++ src/app/services/auth.guard.ts
@@ -1,15 +1,29 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { map, take, tap } from 'rxjs/operators';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+  constructor(private auth: AuthService) { }
+
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    state: RouterStateSnapshot): Observable<boolean> 
+  {
+    return this.auth.user$.pipe(
+      take(1),
+      map(user => !!user),
+      tap(loggedIn => {
+        if(loggedIn) {
+          console.log("unauth'd user tried to access")
+        }
+      })
+      // returns the mapped boolean user
+    )
   }
   
 }
