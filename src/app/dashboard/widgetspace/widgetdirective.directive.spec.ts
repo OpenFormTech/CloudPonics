46d221a7611bed758138bb3ee80d5dc00950e97b
Liam Major <liam.major91@gmail.com>
2021-01-17 16:04:54 -0500

    generated, ignore the error

---
+++ src/app/dashboard/widgetspace/widgetdirective.directive.spec.ts
@@ -0,0 +1,8 @@
+import { WidgetdirectiveDirective } from './widgetdirective.directive';
+
+describe('WidgetdirectiveDirective', () => {
+  it('should create an instance', () => {
+    const directive = new WidgetdirectiveDirective();
+    expect(directive).toBeTruthy();
+  });
+});
