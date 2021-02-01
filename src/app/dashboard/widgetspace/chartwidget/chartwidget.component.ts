02cb8c022954d8b5386a4402ebf0971864fa6ed7
Liam Major <liam.major91@gmail.com>
2021-01-31 21:36:28 -0500

    added chart.js

--- src/app/dashboard/widgetspace/chartwidget/chartwidget.component.ts
+++ src/app/dashboard/widgetspace/chartwidget/chartwidget.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Chart from 'chart.js';
 
 @Component({
   selector: 'app-chartwidget',
@@ -7,9 +8,72 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChartwidgetComponent implements OnInit {
 
-  constructor() { }
+  canvasid: string;
+  chart: Chart;
+  ctx: HTMLElement;
+
+  constructor() { 
+    this.canvasid = Math.random().toString(20).substr(2, 10);
+  }
+
+  // async acquireCanvasElement(): Promise<HTMLElement | void> {
+  //   let r;
+
+  //   for (let i = 0; i < 5; i++) {
+  //     r = document.getElementById(this.canvasid);
+  //     if (r === null) {
+  //       await new Promise(r => setTimeout(r, 2000));
+  //     } else {
+  //       break;
+  //     }
+  //   }
+
+  //   return r;
+  // }
+
+  async makeChart(): Promise<void> {
+    await new Promise(r => setTimeout(r, 200));
+    this.ctx = document.getElementById(this.canvasid);
+    this.chart = new Chart(this.canvasid, {
+        type: 'bar',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+  }
 
   ngOnInit(): void {
+    this.makeChart();
   }
 
 }
