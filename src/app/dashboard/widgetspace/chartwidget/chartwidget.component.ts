76d2d91da08a3dfa23fa1af616ad70058f3dfddc
Liam Major <liam.major91@gmail.com>
2021-02-07 19:44:54 -0500

    Chartwidget no longer constructs a query based on premade input: - ChartWidget gets passed a reference to a collection and the name of a collection to pull data from. - It then queries the collection for all its documents and proceedes map them into the chart
    Removed unused imports e.g. AngularFirestore and removed interface
    Chart Widget has new @Input tags to compensate for the new method of building

--- src/app/dashboard/widgetspace/chartwidget/chartwidget.component.ts
+++ src/app/dashboard/widgetspace/chartwidget/chartwidget.component.ts
@@ -1,11 +1,9 @@
 import { Component, Input, OnInit, ViewChild} from '@angular/core';
-import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 import { AuthService } from '../../../services/auth.service';
-import { Color, BaseChartDirective, Label } from 'ng2-charts';
+import { Color, BaseChartDirective } from 'ng2-charts';
 import { ChartDataSets, ChartOptions, ChartType, ChartPoint } from 'chart.js';
 import 'firebase/firestore';
 import * as moment from 'moment';
-import { dbConfig } from '../widgetspace.interface';
 
 interface DataPoint {
   timestamp: firebase.default.firestore.Timestamp;
@@ -20,53 +18,54 @@ interface DataPoint {
 export class ChartwidgetComponent implements OnInit{
 
   // Input-Populated (User settings)
-  @Input() databaseConfig: dbConfig;          // angular firestore db reference
+  @Input() collectionReference: firebase.default.firestore.DocumentReference;     // angular firestore db reference
   @Input() chartType: ChartType;              // chart type
   @Input() chartOptions : ChartOptions;       // chart options
   @Input() chartLegend = true;                // chart legend input
   @Input() chartColor : Color;                // chart color
   @Input() dataDelimiter : number;            // limiting the amount of data points to pull
+  @Input() dataCollection: string;            // the name of the collection to pull data from
 
   // Database-Populated (from data)
   public chartData: ChartDataSets = {};
 
   // Internal
-  private dataref : AngularFirestoreCollection<DataPoint>;
+  // private dataref : AngularFirestoreCollection<DataPoint>;
   @ViewChild(BaseChartDirective, { static: true }) chart: BaseChartDirective;
 
-  constructor(private db : AngularFirestore, private auth: AuthService) { 
-    this.db = db;
+  constructor(private auth: AuthService) { 
     this.auth = auth;
   }
 
   ngOnInit(): void {
 
+    // CHART CONFIG
+    // since all the data collection names are words separated by dashes, split on the dashes and capitalize each word
+    this.chartData.label = this.dataCollection.split('-').map(word => word.replace(/^\w/, (c) => c.toUpperCase())).join(" ");
+    this.chartData.data = [];
+
+    // my god this looks so fucking nasty holy shit
+
     // GET USER
-    this.auth.getUser().then(user=>{
+    this.auth.getUser().then(_ =>{
       // CREATE LISTENER w/ CUSTOM QUERY
-      this.dataref = this.db.collection<DataPoint>('projects/'+this.databaseConfig.project+'/runs/'+this.databaseConfig.run+'/'+this.databaseConfig.label, collection=>{
-        return collection.orderBy('timestamp').limitToLast(this.dataDelimiter);
-      });
-
-      // CHART CONFIG
-      this.chartData.label = this.databaseConfig.label;
-      this.chartData.data = [];
-
-      // BUILD QUERY-DATA SUBSCRIPTION
-      this.dataref.snapshotChanges(['added']).subscribe(datapoints => {
-        // takings all the data points and mapping them to usable chart data
-        (this.chartData.data as ChartPoint[]) = datapoints.map(datapoint => {
-          const data = datapoint.payload.doc.data();
-          // console.log(data);
-
-          return {
-            x : moment.utc(data.timestamp.toMillis()).local(true),
-            y : data.value
-          };
-        });
-
-        // Refresh the chart
-        this.chart.update();
+      this.collectionReference.collection(this.dataCollection)
+        .orderBy('timestamp')               // ordering the collection be sorted by timestamp
+        .limitToLast(this.dataDelimiter)    // limiting N points
+        .onSnapshot({                       // subscribing to datastream
+          next: (data) => {
+            // the net data collection
+            data.forEach(v => {
+              // pushing the new data to our chartdata array
+              (this.chartData.data as ChartPoint[]).push(
+                // values to ones which are readable by chart js
+                {
+                  x: moment.utc(v.data().timestamp.toMillis()).local(true),
+                  y: v.data().value
+                }
+              );
+              this.chart.update();
+            })}
       });
     });
   }
